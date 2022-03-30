1eff4bd0ef4243631b0577c811a4ad9fad10f644
Jonathas Tavares <jonathastavares@id.uff.br>
2022-03-30 17:11:13 -0300

    Setup rack-cors gem to accept fetchs from all origins

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,7 @@
 class GreetingsController < ActionController::API
     def index
         @greeting = Greeting.all.sample
+        render json: @greeting
         return @greeting.to_json
     end
 end
